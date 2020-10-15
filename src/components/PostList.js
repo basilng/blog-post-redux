773feac617d7b9d40e6debde69980e46947f7761
Basil N G <basilng.bng@gmail.com>
2020-10-16 02:49:54 +0530

    Wiring up an action creator

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,13 @@
 import React from "react";
-
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
   render() {
-    return (
-      <div>
-        <h1>sds</h1>
-      </div>
-    );
+    return <div></div>;
   }
 }
 
-export default PostList;
+export default connect(null, { fetchPosts })(PostList);
